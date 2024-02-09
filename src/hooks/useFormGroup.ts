a869784335024453eafd3e9a51d90e96bfd78748
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-09 23:50:46 +0100

    css reset

--- src/hooks/useFormGroup.ts
+++ src/hooks/useFormGroup.ts
@@ -1,8 +1,24 @@
 import { useState } from "react"
-import { FormGroup } from "../components/FormGroup"
+import InputControl from "../components/InputControl"
+import { ValidatorFn, ValidatorFns } from "../types/validatorsTypes"
 
-function useFormGroup(initialFormGroup : FormGroup){
+function useFormGroup(){
+
+    const initialFormGroup = {
+        controls : {},  
+        getControls : function () { return this.controls },
+        addControl : function (fieldArgs : {accessor : string, defaultValue : string, isRequired : boolean, validationFns : ValidatorFn | ValidatorFns | undefined}) {
+            if (fieldArgs == null) return this
+            this.controls = {...this.controls,
+                [fieldArgs.accessor] : new InputControl(fieldArgs)
+            }
+            return this
+        },
+        
+    }
+
     const [formGroup, setFormGroup]= useState(initialFormGroup)
+
 
     function setControlValue({controlName, value} : {controlName : string, value : string}){
         const _formGroup = {...formGroup, controls : {...formGroup.controls, [controlName] : value }}
